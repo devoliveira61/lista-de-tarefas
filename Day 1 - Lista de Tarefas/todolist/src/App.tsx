e2f50db5dc6ba334ac104a5b8e133145930d86d7
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-06-16 18:14:59 -0300

    Toda a funcionalidade de marcar e desmarcar está feita

--- Day 1 - Lista de Tarefas/todolist/src/App.tsx
+++ Day 1 - Lista de Tarefas/todolist/src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/item";
+import { ListItem } from "./components/ListItem";
+import { AddArea } from './components/AddArea'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Estudar React", done: false },
+    { id: 2, name: "Estudar Estrutura de Dados", done: true },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea />
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
   );
