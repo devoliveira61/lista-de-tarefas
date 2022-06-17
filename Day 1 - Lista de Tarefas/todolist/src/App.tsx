668f1ce2906da20fd3438a7354bd188f2a0986ee
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-06-16 21:27:46 -0300

    Projeto finalizado

--- Day 1 - Lista de Tarefas/todolist/src/App.tsx
+++ Day 1 - Lista de Tarefas/todolist/src/App.tsx
@@ -2,27 +2,52 @@ import { useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/item";
 import { ListItem } from "./components/ListItem";
-import { AddArea } from './components/AddArea'
+import { AddArea } from "./components/AddArea";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: "Estudar React", done: false },
-    { id: 2, name: "Estudar Estrutura de Dados", done: true },
+    // { id: 1, name: "Estudar React", done: false },
+    // { id: 2, name: "Estudar Estrutura de Dados", done: true },
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  };
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
 
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        <AddArea />
+        <AddArea onEnter={handleAddTask} />
 
-        {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+        {list.map((item, index)=>(
+          <ListItem
+            key={index}
+            item={item}
+            onChange={handleTaskChange}
+          />
         ))}
+
       </C.Area>
     </C.Container>
-  );
-};
+);
+}
 
 export default App;
